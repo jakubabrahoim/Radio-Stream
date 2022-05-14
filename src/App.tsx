83b3778c9ec452eb52bd7bc6f0815d059ffa464c
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-05-14 19:52:50 +0200

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { BrowserRouter } from 'react-router-dom';
+
+import Navigation from './components/Navigation';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let queryClient = new QueryClient();
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+                <h1>React app 😎</h1>
+            </div>
+            
+            <BrowserRouter>
+                <Navigation />
+            </BrowserRouter>
+
+            <ReactQueryDevtools/>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,9 @@
+function Navigation() {
+    return (
+        <div>
+            <h1>Navigation will be here </h1>
+        </div>
+    )
+}
+
+export default Navigation;
