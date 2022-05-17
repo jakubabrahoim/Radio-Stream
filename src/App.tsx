dbfa4dc8fc7154ee4afa70eb6fd79200a48ccda8
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-05-17 21:08:30 +0200

    more firebase fixes

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ const firebaseConfig = {
   
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const analytics = getAnalytics(app);
 
 function App() {
