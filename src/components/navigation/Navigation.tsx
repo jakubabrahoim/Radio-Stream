b695c2e29d459c9b49b47bdc160e155d4ef4901b
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-05-20 20:06:29 +0200

    added the option to log in

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -10,7 +10,7 @@ function Navigation() {
         let auth = getAuth();
         onAuthStateChanged(auth, user => {
             if(user) {
-                console.log(user);
+                //console.log(user);
                 setUser(user);
             }
         });
