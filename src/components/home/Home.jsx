7259498db6a8d979df0ad599899128ba9d09cbc9
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-05-28 23:31:35 +0200

    changed font and added metadata

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -13,12 +13,14 @@ function Home(props: Props) {
                 <section className='self-center'>
                     <form className='flex flex-row' onSubmit={e => e.preventDefault()}>
                         <input className='w-96 h-12 mr-4 px-2 border rounded-lg drop-shadow-md' type='text' placeholder='Search for radio stations...'></input>
-                        <input className='w-24 px-2 text-white bg-gray-800 hover:bg-gray-700 hover:cursor-pointer rounded-lg' type='submit' name="search" value='Search'></input>
+                        <input className='w-24 px-2 text-white bg-gray-800 hover:bg-gray-700 hover:cursor-pointer rounded-lg drop-shadow-md' type='submit' name="search" value='Search'></input>
                     </form>
                 </section>
                 
             </article>
             
+            <h1 className='text-center mb-10 text-xl'>Popular stations from your country</h1>
+
             <article className='grid grid-flow-row grid-rows-1 justify-center align-middle'>
                 <section className='flex flex-row items-center'>
                     <div className='mx-6 h-60 w-60 border rounded-lg'>
