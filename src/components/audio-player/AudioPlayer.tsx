3ac93ac1b85aaf273a33804a309c284856b403f8
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-06-06 18:20:50 +0200

    updated audio player - added mute button, changed play button color

--- src/components/audio-player/AudioPlayer.tsx
+++ src/components/audio-player/AudioPlayer.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+
 import { BiRadio } from "react-icons/bi";
 import { BsPlayFill, BsStopFill } from "react-icons/bs";
 import { AiOutlineHeart, AiFillHeart, AiOutlineLoading } from "react-icons/ai";
+import { BsVolumeMuteFill, BsFillVolumeUpFill } from "react-icons/bs";
 import { IconContext } from "react-icons";
-import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 import { Tooltip } from '@mantine/core';
 
 function AudioPlayer() {
@@ -16,6 +18,7 @@ function AudioPlayer() {
     let streamUrl: string = 'https://radioshamfm.grtvstream.com:8400/;';
     let [audio, setAudio] = useState(new Audio(streamUrl));
     let [audioVolume, setAudioVolume] = useState(50);
+    let [muted, setMuted] = useState({muted: false, volumeBeforeMute: 50})
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     let [stationName, setStationName] = useState('Sham FM');
 
@@ -65,8 +68,19 @@ function AudioPlayer() {
         audio.volume = newVolume / 100;
     }
 
+    function muteAudio() {
+        if(muted.muted === true) {
+            audio.volume = muted.volumeBeforeMute / 100;
+            setMuted({muted: false, volumeBeforeMute: audioVolume});
+        } else {
+            setMuted({muted: true, volumeBeforeMute: audioVolume});
+            audio.volume = 0;
+        }
+    }
+
     return (
         <div className='grid grid-cols-3 gap-10 justify-items-center items-center fixed bottom-0 w-screen h-20 border-t border-gray-800 bg-gray-100'>
+            {/* Radio name + radio thumbnail */}
             <div className='flex flex-row items-center'>
                 <div className='w-14 h-14 bg-gray-300 rounded-lg mr-6 flex items-center justify-center'>
                     {
@@ -79,11 +93,10 @@ function AudioPlayer() {
                 </div>
                 <h1>{stationName}</h1>
             </div>
-        
-            <div className=''>
-                {/* @ts-ignore */}
-                {/*<audio ref={audioRef} src={streamUrl}></audio>*/}
-                <button onClick={playStream} disabled={audioPlaying === 'loading'} className={`rounded-full border border-gray-800 bg-gray-700 hover:bg-gray-500 w-12 h-12 flex items-center justify-center ${audioPlaying === 'loading' ? 'cursor-progress' : ''}`}>
+            
+            {/* Radio play button */}
+            <div>
+                <button onClick={playStream} disabled={audioPlaying === 'loading'} className={`rounded-full border border-gray-800 bg-gray-800 hover:bg-gray-700 w-12 h-12 flex items-center justify-center ${audioPlaying === 'loading' ? 'cursor-progress' : ''}`}>
                     {
                         audioPlaying === 'stopped' &&
                         <IconContext.Provider value={{ className: 'text-white pl-0.5 w-6 h-6' }}>
@@ -105,13 +118,23 @@ function AudioPlayer() {
                     
                 </button>
             </div>
-
+            
+            {/* Radio volume + like button */}
             <div className='flex flex-row items-center'>
                 <div className='mr-10'>
-                    <label className='text-sm'>Volume</label>
+                    <div className='flex flex-row items-center'>
+                        <p className='text-sm mr-14'>Volume</p>
+                        <button onClick={muteAudio} className='rounded-full hover:bg-gray-200 w-8 h-8 pl-1.5'>
+                            <IconContext.Provider value={{className: 'h-5 w-5'}}>
+                                { muted.muted === false ? <BsFillVolumeUpFill/> : <BsVolumeMuteFill/> }
+                            </IconContext.Provider>
+                        </button>
+                    </div>
+                    <label className='text-sm' hidden>Volume</label>
                     <input type='range' min='0' max='100' step='1' value={audioVolume} onChange={handleVolumeChange} className='w-full accent-gray-800'/>
                 </div>
                 
+                {/* Logged in, verified user -> can like stations */}
                 {
                     (user !== null) && (verified === true) &&
                     <>
@@ -132,6 +155,7 @@ function AudioPlayer() {
                     </>
                 }
 
+                {/* Logged in, unverified user -> can't like stations -> show tooltip */}
                 {
                     (user !== null) && (verified === false) &&
                     <Tooltip wrapLines position='left' width={200} withArrow transition='fade' transitionDuration={200} label='Your account must be verified to like stations.'>
@@ -151,7 +175,6 @@ function AudioPlayer() {
                         </button>
                     </Tooltip>
                 }
-
             </div>
         </div>
     );
